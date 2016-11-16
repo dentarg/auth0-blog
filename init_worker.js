40e81ca1096653cc8cac6d28e160bce485379e76
Miguel Casas <neneriostb@gmail.com>
2016-11-16 13:35:53 -0500

    subscribe pushnotification validate

--- init_worker.js
+++ init_worker.js
@@ -29,59 +29,72 @@ window.requestNotificationPermission = function () {
 function registerForPush() {
     //if (navigator.serviceWorker.controller) {
         navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-            serviceWorkerRegistration.pushManager.subscribe({
-                    userVisibleOnly: true
-                })
-                .then(function(subscription) {
-                    fetch("https://auth0-marketing.run.webtask.io/blog-push-subscriptions?webtask_no_cache=1",
-                    {
-                        headers: {
-                          'Accept': 'application/json',
-                          'Content-Type': 'application/json'
-                        },
-                        method: "POST",
-                        body: JSON.stringify({"registration_id": subscription.endpoint.substr(subscription.endpoint.lastIndexOf('/') + 1)})
-                    })
-                    .then(function(res){ console.log('registered') })
-                    .catch(function(err){ console.err(err); return });
-                    console.log("DEVICE_REGISTRATION_ID: ", subscription.endpoint.substr(subscription.endpoint.lastIndexOf('/') + 1));
-                })
-                .catch(function(error) {
-                    console.log("Subscription for Push failed", error);
-                });
+
+            serviceWorkerRegistration.pushManager.getSubscription().then(
+                function(pushSubscription) {
+                // Check we have a subscription to unsubscribe
+                    if (pushSubscription) {
+                        console.warn('subscribed Notification')
+                        return;
+                    }
+                    subscribe(serviceWorkerRegistration);
+            });
         });
     //} else {
       // console.log("No active ServiceWorker");
     //}
 }
 
-window.unsubscribePushNotification = function() {
-  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+function subscribe(serviceWorkerRegistration){
+    return serviceWorkerRegistration.pushManager.subscribe({
+        userVisibleOnly: true
+    })
+    .then(function(subscription) {
+        fetch("https://auth0-marketing.run.webtask.io/blog-push-subscriptions?webtask_no_cache=1",
+        {
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({"registration_id": subscription.endpoint.substr(subscription.endpoint.lastIndexOf('/') + 1)})
+        })
+        .then(function(res){ console.log('registered') })
+        .catch(function(err){ console.err(err); return });
+        console.log("DEVICE_REGISTRATION_ID: ", subscription.endpoint.substr(subscription.endpoint.lastIndexOf('/') + 1));
+    })
+    .catch(function(error) {
+        console.log("Subscription for Push failed", error);
+    });
+}
 
-    serviceWorkerRegistration.pushManager.getSubscription().then(
-      function(pushSubscription) {
-        // Check we have a subscription to unsubscribe
-        if (!pushSubscription) {
-            console.warn('Unsubscribed Notification')
-            return;
-        }
+window.unsubscribePushNotification = function () {
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
 
-        var subscriptionId = pushSubscription.endpoint.substr(pushSubscription.endpoint.lastIndexOf('/') + 1);
-        // request to wt to remove subscription of db
-        pushSubscription.unsubscribe().then(function(successful) {
-            fetch("https://auth0-marketing.run.webtask.io/blog-push-subscriptions/"+subscriptionId,
-            {
-                method: "DELETE"
-            })
-            .then(function(res){ console.log('unsubscribe') })
-            .catch(function(err){ console.err(err); return });
+        serviceWorkerRegistration.pushManager.getSubscription().then(
+            function(pushSubscription) {
+            // Check we have a subscription to unsubscribe
+                if (!pushSubscription) {
+                    console.warn('Unsubscribed Notification')
+                    return;
+                }
+
+                var subscriptionId = pushSubscription.endpoint.substr(pushSubscription.endpoint.lastIndexOf('/') + 1);
+                // request to wt to remove subscription of db
+                pushSubscription.unsubscribe().then(function(successful) {
+                    fetch("https://auth0-marketing.run.webtask.io/blog-push-subscriptions/"+subscriptionId,
+                    {
+                        method: "DELETE"
+                    })
+                    .then(function(res){ console.log('unsubscribe') })
+                    .catch(function(err){ console.err(err); return });
+                }).catch(function(e) {
+                    console.log('Unsubscription error: ', e);
+                });
         }).catch(function(e) {
-          console.log('Unsubscription error: ', e);
+            console.error('Error thrown while unsubscribing from push messaging.', e);
         });
-      }).catch(function(e) {
-        console.error('Error thrown while unsubscribing from push messaging.', e);
-      });
-  });
+    });
 }
 
 function doesBrowserSupportNotifications() {
